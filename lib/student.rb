0294bf964881322c89890dcf8eddd7fcda3bc975
Arthur Song <arth3rs0ng@gmail.com>
2019-04-08 02:58:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,10 @@ class Student < User
   attr_accessor :knowledge
   
   def initialize
-    @
-    
-    knowledge = []
+    @knowledge = []
   end
   
-  
+  def learn(info)
+    @knowledge << info
+  end
 end
